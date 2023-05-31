0664cfa031f3c6ddc13212f76f06c570b690ac26
marsh31 <marsh22h53@gmail.com>
2023-05-31 21:40:03 +0900

    fix: refactor

--- lua/nvim_qf_helper/open/init.lua
+++ lua/nvim_qf_helper/open/init.lua
@@ -1,57 +1,13 @@
 local M = {}
 
-local opner = {
-  "vnew",
-  "new",
-  "split",
-  "vsplit",
-  "tabnew",
-  "botright",
-  "topleft",
-  "rightbelow",
-  "leftabove",
-  "vertical",
-}
+M.open = function(opener, info)
+  if info.bufnr ~= nil and info.lnum ~= nil and info.col ~= nil then
+    vim.cmd(("%s %s"):format(opener, vim.fn.bufname(info.bufnr)))
+    vim.fn.cursor { info.lnum, info.col }
 
-local function get_type(winid)
-  local info = vim.fn.getwininfo(winid)
-
-  if not info or not info[1].loclist or not info[1].quickfix then
-    return "n"
-  end
-
-  if info[1].loclist == 1 then
-    return "l"
-  elseif info[1].quickfix == 1 then
-    return "c"
   else
-    return "n"
+    warn("info is invalid.")
   end
-end
-
-M.open = function(opener)
-  local winid = vim.fn.win_getid()
-  local type = get_type(winid)
-
-  local list
-
-  if type == "n" then
-    list = nil
-  elseif type == "c" then
-    list = vim.fn.getqflist()
-  elseif type == "l" then
-    list = vim.fn.getloclist(winid)
-  end
-
-  if not list then
-    warn "Quickfix window is not exist"
-    return
-  end
-
-
-  local info = list[vim.fn.line(".")]
-  vim.cmd(("%s %s"):format(opener, vim.fn.bufname(info.bufnr)))
-  vim.fn.cursor(info.lnum, info.col)
 end
 
 -- @param msg string
