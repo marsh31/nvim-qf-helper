9cd8701bd641e8fd5fc442cbc0a2ca99be8713e4
marsh31 <marsh22h53@gmail.com>
2023-05-31 21:40:45 +0900

    add: open cmd ex: edit, split, vsplit

--- lua/nvim_qf_helper/main.lua
+++ lua/nvim_qf_helper/main.lua
@@ -31,6 +31,29 @@ function NvimQfHelper:disable()
   vim.api.nvim_clear_autocmds { group = self.augroup }
 end
 
+local function open(open_cmd)
+  local info = require("nvim_qf_helper.quickfix").get_qf_info_by_current_line()
+
+  if info ~= nil then
+    local selector = require("nvim_qf_helper.selector").new()
+    selector:select(function()
+      require("nvim_qf_helper.open").open(open_cmd, info)
+    end)
+  end
+end
+
+function NvimQfHelper:edit()
+  open "edit"
+end
+
+function NvimQfHelper:split()
+  open "split"
+end
+
+function NvimQfHelper:vsplit()
+  open "vsplit"
+end
+
 -- @param msg string
 -- @return nil
 function NvimQfHelper:warn(msg)
